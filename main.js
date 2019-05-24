273ac64e54d60a4b58caad9c38778f3a519536d4
Усоян Шамил <shamila2@yandex.ru>
2019-05-24 17:01:59 +0300

    Update: private key, value from selector

--- main.js
+++ main.js
@@ -1,65 +1,28 @@
+class KeyStorage {
+  getKey = function() {
+    return "trnsl.1.1.20170408T091129Z.c48f89c680ca9072.9a307807964083741a2848110b90a7c7fe4d525a";
+  };
+}
+
 window.addEventListener("DOMContentLoaded", function() {
   const btn = document.getElementById("btn"),
     txt1 = document.getElementById("txt1"),
     txt2 = document.getElementById("txt2");
   btn.addEventListener("click", function() {
-    let firstLang = document.getElementById("firstLang").options.selectedIndex;
-    let secondLang = document.getElementById("secondLang").options
-      .selectedIndex;
-    let firstLangStr = "";
-    let secondLangStr = "";
-    switch (firstLang) {
-      case 0:
-        firstLangStr = "en";
-        break; //Английский  en
-      case 1:
-        firstLangStr = "ru";
-        break; //Русский     ru
-      case 2:
-        firstLangStr = "de";
-        break; //Немецкий    de
-      case 3:
-        firstLangStr = "es";
-        break; //Испанский   es
-      case 4:
-        firstLangStr = "zh";
-        break; //Китайский   zh
-      default:
-        firstLangStr = "en";
-    }
-    switch (secondLang) {
-      case 0:
-        secondLangStr = "en";
-        break;
-      case 1:
-        secondLangStr = "ru";
-        break;
-      case 2:
-        secondLangStr = "de";
-        break;
-      case 3:
-        secondLangStr = "es";
-        break;
-      case 4:
-        secondLangStr = "zh";
-        break;
-      default:
-        secondLangStr = "en";
-    }
-
+    let firstLang = document.getElementById("firstLang");
+    let secondLang = document.getElementById("secondLang");
     let request = new XMLHttpRequest();
     let text = encodeURIComponent(txt1.value);
-    const key =
-      "trnsl.1.1.20170408T091129Z.c48f89c680ca9072.9a307807964083741a2848110b90a7c7fe4d525a";
+    let key = new KeyStorage();
     const url =
       "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" +
-      key +
+      key.getKey() +
       "&text=" +
       text +
       "&lang=" +
-      firstLangStr +
+      firstLang.value +
       "-" +
-      secondLangStr +
+      secondLang.value +
       "&format=plain&options=1";
     request.open("GET", url, true);
     request.onload = function() {
